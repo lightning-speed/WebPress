a0ccfd7eefd5d69ad5ca332cc44f9bfa8f77109c
peko <82322282+lightning-speed@users.noreply.github.com>
2022-04-30 16:51:52 +0530

    Add files via upload

--- abc.php
+++ abc.php
@@ -1,3 +1,25 @@
-<?php 
-     echo '<p>Hello World</p>' 
-?> 
+<?php
+
+$servername = "localhost";
+$username = "abc";
+$password = "abc123";
+$dbname = "main";
+
+// Create connection
+$conn = new mysqli($servername,
+	$username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: "
+		. $conn->connect_error);
+}
+
+$sqlquery = "INSERT INTO table VALUES
+	('John', 'Doe', 'john@example.com')"
+
+if ($conn->query($sql) === TRUE) {
+	echo "record inserted successfully";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
